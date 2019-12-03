7cb453505e548eade6174cd9c064d2432a6005fa
Fabb's <fesquivel@asgbpm.com>
2019-12-03 14:14:47 -0600

    Practica #4 Pipes Completo

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -8,8 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Pipes';
   nombre = 'Fabiola';
+  nombre2 = 'faBIola esquIVel teLLez';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   Pi = Math.PI;
   a = 0.234;
   salario = 1234.5;
+
+  heroe = {
+    nombre: 'Clark Kent',
+    clave: 'Superman',
+    edad: '30',
+    direccion: {
+      calle: 'Kripton',
+      casa: 'Hielo'
+    }
+  };
+
+  // tslint:disable-next-line: no-shadowed-variable
+  valorDePromesa = new Promise(( resolve, reject ) => {
+    setTimeout( () => resolve('Llego la data'), 3500);
+  });
+
+  fecha = new Date();
+  video = 'Ahha3Cqe_fk';
+  activar = true;
 }
